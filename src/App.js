cdcddc8c7854b0faf2e234807d073b256529b445
HXXNS <39939869+HXXNS@users.noreply.github.com>
2020-01-22 23:24:03 +0900

    #2.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Potato from './Potato';
+
+
+function Food({fav}) {
+  return <h1>I like {fav}</h1>
+}
 
 function App() {
   return (
     <div>
       <h3>hello</h3>
-      <Potato />
+      <Food fav="kimchi"/>
+      <Food fav="ramen"/>
+      <Food fav="samgiopsal"/>
+      <Food fav="chukumi"/>
     </div>
   );
 }
